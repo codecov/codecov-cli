ba8c4f84fa6647bcd0dccd595e938aea613ddd21
Thiago <44376991+ThiagoCodecov@users.noreply.github.com>
2022-04-06 23:56:10 -0300

    Create python-package.yml

--- tests/test_codecov_cli.py
+++ tests/test_codecov_cli.py
@@ -1,5 +1,14 @@
 from codecov_cli import __version__
+from codecov_cli import main
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_existing_commands():
+    assert sorted(main.cli.commands.keys()) == [
+        "create-commit",
+        "create-report",
+        "do-upload",
+    ]

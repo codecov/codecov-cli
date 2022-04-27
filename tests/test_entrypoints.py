226b62166f14b3d8a8fb927c194c750333ba3e05
Ahmed-Elghrbawy <ahmed@codecov.io>
2022-04-27 21:41:04 +0200

    add missing paramter to UploadCollectionResult after mergin master

--- tests/test_entrypoints.py
+++ tests/test_entrypoints.py
@@ -14,7 +14,7 @@ from codecov_cli import __version__ as codecov_cli_version
 class TestUploadSender(object):
     @pytest.fixture
     def fake_upload_data(self):
-        upload_collection = UploadCollectionResult(["1", "apple.py", "3"], [])
+        upload_collection = UploadCollectionResult(["1", "apple.py", "3"], [], [])
         random_token = uuid.UUID("f359afb9-8a2a-42ab-a448-c3d267ff495b")
         random_sha = "845548c6b95223f12e8317a1820705f64beaf69e"
         return (upload_collection, random_token, random_sha)

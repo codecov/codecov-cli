c41410e298a862b3156562577f13d278ef4d0417
Ahmed-Elghrbawy <ahmed@codecov.io>
2022-05-18 15:28:56 +0200

    modify git helper to raise valueError for invalid addresses and add tests

--- codecov_cli/helpers/git.py
+++ codecov_cli/helpers/git.py
@@ -1,8 +1,15 @@
 def parse_slug(address: str):
     if 'http' in address:
-        return address.split("//")[1].split("/", 1)[1].split(".git")[0]
+        try:
+            return address.split("//")[1].split("/", 1)[1].split(".git")[0]
+        except IndexError:
+            raise ValueError("Argument address is not a valid address")
+            
     
     if '@' in address:
-        return address.split(":")[1].split(".git")[0]
+        try:
+            return address.split(":")[1].split(".git")[0]
+        except IndexError:
+            raise ValueError("Argument address is not a valid address")
     
-    raise ValueError("Argument address is not a valid address.")
+    raise ValueError("Argument address is not a valid address")
---
+++ tests/test_git.py
@@ -0,0 +1,28 @@
+from codecov_cli.helpers import git
+import pytest
+
+
+@pytest.mark.parametrize(
+    "address",
+    [
+        ("origin    https://github.com/codecov/codecov-cli.git (fetch)"),
+        ("https://github.com/codecov/codecov-cli.git"),
+        ("origin  git@github.com:codecov/codecov-cli.git (fetch)"),
+        ("git@github.com:codecov/codecov-cli.git"),
+    ],
+)
+def test_parse_slug_valid_address(address):
+    assert git.parse_slug(address) == "codecov/codecov-cli"
+    
+
+@pytest.mark.parametrize(
+    "address",
+    [
+        ("https://github.com"),
+        ("test@test.org"), 
+        ("aa")       
+    ],
+)  
+def test_parse_slug_invalid_address(address):
+    with pytest.raises(ValueError):
+        assert git.parse_slug(address)

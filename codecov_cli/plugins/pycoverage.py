9459de3e8ddf9ff7b953b4659850ac84bcd6b579
Ahmed-Elghrbawy <ahmed@codecov.io>
2022-05-23 19:43:26 +0200

    refactor pycoverage to use click.echo instead of print

--- codecov_cli/plugins/pycoverage.py
+++ codecov_cli/plugins/pycoverage.py
@@ -3,6 +3,7 @@ import pathlib
 import shutil
 import subprocess
 import typing
+import click
 from glob import iglob
 
 
@@ -11,11 +12,11 @@ class Pycoverage(object):
         self.project_root = project_root or pathlib.Path(os.getcwd())
 
     def run_preparation(self, collector):
-        print("Running coverage.py plugin...")
+        click.echo("Running coverage.py plugin...")
 
         if shutil.which("coverage") is None:
-            print("coverage.py is not installed or can't be found.")
-            print("aborting coverage.py plugin...")
+            click.echo("coverage.py is not installed or can't be found.")
+            click.echo("aborting coverage.py plugin...")
             return
 
         # This might need optimization
@@ -27,14 +28,14 @@ class Pycoverage(object):
         )
 
         if path_to_coverage_data is None:
-            print("No coverage data found.")
-            print("aborting coverage.py plugin...")
+            click.echo("No coverage data found.")
+            click.echo("aborting coverage.py plugin...")
             return
 
         coverage_data_directory = pathlib.Path(path_to_coverage_data).parent
         self._generate_XML_report(coverage_data_directory)
 
-        print("aborting coverage.py plugin...")
+        click.echo("aborting coverage.py plugin...")
 
     def _generate_XML_report(self, dir):
         """Generates up-to-date XML report in the given directory"""
@@ -42,14 +43,14 @@ class Pycoverage(object):
         # the following if conditions avoid creating dummy .coverage file
 
         if next(iglob(os.path.join(dir, ".coverage.*")), None) is not None:
-            print(f"Running coverage combine -a in {dir}")
+            click.echo(f"Running coverage combine -a in {dir}")
             subprocess.run(["coverage", "combine", "-a"], cwd=dir)
 
         if os.path.exists(os.path.join(dir, ".coverage")):
-            print(f"Generating coverage.xml report in {dir}")
+            click.echo(f"Generating coverage.xml report in {dir}")
             completed_process = subprocess.run(
                 ["coverage", "xml", "-i"], cwd=dir, capture_output=True
             )
 
             output = completed_process.stdout.decode().strip()
-            print(output)
+            click.echo(output)

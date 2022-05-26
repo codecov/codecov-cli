ebf4fab75ab725a466b6f950ed60370a62615bbd
Ahmed-Elghrbawy <ahmed@codecov.io>
2022-05-26 14:48:28 +0200

    add docstring for git.py helper

--- codecov_cli/helpers/git.py
+++ codecov_cli/helpers/git.py
@@ -1,4 +1,13 @@
 def parse_slug(address: str):
+    """Extracts a slug from git based urls"""
+
+    """
+    URL examples:
+        origin    https://github.com/codecov/codecov-cli.git (fetch)
+        https://github.com/codecov/codecov-cli.git
+        origin  git@github.com:codecov/codecov-cli.git (fetch)
+        git@github.com:codecov/codecov-cli.git
+    """
     if "http" in address:
         try:
             return address.split("//")[1].split("/", 1)[1].split(".git")[0]

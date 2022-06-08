0357ec5bc8a32647fa8d03de1e4e5d4209ab496e
Ahmed-Elghrbawy <ahmed@codecov.io>
2022-06-08 18:47:33 +0200

    seperate git fallbacks and git helper into seperate PRs

--- codecov_cli/helpers/versioning_systems.py
+++ codecov_cli/helpers/versioning_systems.py
@@ -26,43 +26,6 @@ class GitVersioningSystem(VersioningSystemInterface):
             if p.stdout:
                 return p.stdout.decode().strip()
 
-        if fallback_field == FallbackFieldEnum.branch:
-            p = subprocess.run(
-                ["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True
-            )
-            if p.stdout:
-                branch_name = p.stdout.decode().strip()
-                # branch_name will be 'HEAD' if we are in 'detached HEAD' state
-                return branch_name if branch_name != "HEAD" else None
-
-        if fallback_field == FallbackFieldEnum.slug:
-            # if there are multiple remotes, we will prioritize using the one called 'origin' if it exsits, else we will use the first one in 'git remote' list
-
-            p = subprocess.run(["git", "remote"], capture_output=True)
-
-            if not p.stdout:
-                return None
-
-            remotes = p.stdout.decode().strip().splitlines()
-
-            remote_name = "origin" if "origin" in remotes else remotes[0]
-
-            p = subprocess.run(
-                ["git", "ls-remote", "--get-url", remote_name], capture_output=True
-            )
-            if not p.stdout:
-                return None
-
-            remote_url = p.stdout.decode().strip()
-
-            return parse_slug(remote_url)
-
-        return None
-
-    def get_network_root(self):
-        p = subprocess.run(["git", "rev-parse", "--show-toplevel"], capture_output=True)
-        if p.stdout:
-            return Path(p.stdout.decode().rstrip())
         return None
 
     def list_relevant_files(self):

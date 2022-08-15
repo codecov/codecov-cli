592e72988bd4da6d10b635535c0a91217ccb47d0
Dana <dana@codecov.io>
2022-08-15 19:13:41 +0300

    test for searching for directories

--- tests/helpers/test_folder_searcher.py
+++ tests/helpers/test_folder_searcher.py
@@ -252,3 +252,34 @@ def test_globs_to_regex_returns_none_if_patterns_empty():
     regex = globs_to_regex([])
 
     assert regex is None
+
+
+def test_search_directories(tmp_path):
+    filename_include_regex = globs_to_regex(["*.app"])
+    filepaths = [
+        "banana.app/path/of/directory.txt",
+        "path/to/apple.app/path/of/directorys",
+        "path/to/banana.app/folder/test.txt",
+        "apple.py",
+        "banana.py",
+    ]
+    for f in filepaths:
+        relevant_filepath = tmp_path / f
+        relevant_filepath.parent.mkdir(parents=True, exist_ok=True)
+        relevant_filepath.touch()
+    expected_results = sorted(
+        [
+            tmp_path / "banana.app",
+            tmp_path / "path/to/banana.app",
+            tmp_path / "path/to/apple.app",
+        ]
+    )
+    assert expected_results == sorted(
+        search_files(
+            tmp_path,
+            [],
+            filename_include_regex=filename_include_regex,
+            filename_exclude_regex=None,
+            search_for_directories=True,
+        )
+    )

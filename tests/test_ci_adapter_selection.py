01a9e3e7e9fffd93b5d76fb96e7c5b42bc6a74ec
Gguidini <giovanni@codecov.io>
2023-01-23 14:32:45 -0300

    Organize all ci_adapter unit tests in the same folder
    We had a `ci_adapters` folder under `tests` with some tests, but tests to other adapters where under `test_ci_adapters.py` directly.
    This makes it harder to keep track of which CIs we have tests for and where they are.
    
    This is a simple attempt at putting everything in the same place separatedly.
    THe old `test_ci_adapters.py` contains now only the selection tests.

---
+++ tests/test_ci_adapter_selection.py
@@ -0,0 +1,30 @@
+import os
+from enum import Enum
+
+import pytest
+
+from codecov_cli.fallbacks import FallbackFieldEnum
+from codecov_cli.helpers.ci_adapters import (
+    AppveyorCIAdapter,
+    CircleCICIAdapter,
+    GithubActionsCIAdapter,
+    GitlabCIAdapter,
+    get_ci_adapter,
+)
+
+
+class TestCISelector(object):
+    def test_returns_none_if_name_is_invalid(self):
+        assert get_ci_adapter("random ci adapter name") is None
+
+    def test_returns_circleCI(self):
+        assert isinstance(get_ci_adapter("circleci"), CircleCICIAdapter)
+
+    def test_returns_githubactions(self):
+        assert isinstance(get_ci_adapter("githubactions"), GithubActionsCIAdapter)
+
+    def test_returns_gitlabCI(self):
+        assert isinstance(get_ci_adapter("gitlabCI"), GitlabCIAdapter)
+
+    def test_returns_appveyor(self):
+        assert isinstance(get_ci_adapter("appveyor"), AppveyorCIAdapter)

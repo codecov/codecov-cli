7f22624f1a7c540b5945bc20430fb49cded3cfde
Dana <dana@codecov.io>
2023-02-02 18:56:05 +0200

    remove forgotten for loop

--- codecov_cli/helpers/ci_adapters/__init__.py
+++ codecov_cli/helpers/ci_adapters/__init__.py
@@ -20,35 +20,34 @@ logger = logging.getLogger("codecovcli")
 
 def get_ci_adapter(provider_name: str = None):
     if provider_name:
-        for provider in get_ci_providers_list():
-            if provider_name == "circleci":
-                return CircleCICIAdapter()
-            if provider_name == "githubactions":
-                return GithubActionsCIAdapter()
-            if provider_name == "gitlabCI":
-                return GitlabCIAdapter()
-            if provider_name == "bitbucket":
-                return BitbucketAdapter()
-            if provider_name == "bitrise":
-                return BitriseCIAdapter()
-            if provider_name == "appveyor":
-                return AppveyorCIAdapter()
-            if provider_name == "local":
-                return LocalAdapter()
-            if provider_name == "woodpecker":
-                return WoodpeckerCIAdapter()
-            if provider_name == "heroku":
-                return HerokuCIAdapter()
-            if provider_name == "droneci":
-                return DroneCIAdapter()
-            if provider_name == "buildkite":
-                return BuildkiteAdapter()
-            if provider_name == "azurepipelines":
-                return AzurePipelinesCIAdapter()
-            if provider_name == "jenkins":
-                return JenkinsAdapter()
-            if provider_name == "cirrusci":
-                return CirrusCIAdapter()
+        if provider_name == "circleci":
+            return CircleCICIAdapter()
+        if provider_name == "githubactions":
+            return GithubActionsCIAdapter()
+        if provider_name == "gitlabCI":
+            return GitlabCIAdapter()
+        if provider_name == "bitbucket":
+            return BitbucketAdapter()
+        if provider_name == "bitrise":
+            return BitriseCIAdapter()
+        if provider_name == "appveyor":
+            return AppveyorCIAdapter()
+        if provider_name == "local":
+            return LocalAdapter()
+        if provider_name == "woodpecker":
+            return WoodpeckerCIAdapter()
+        if provider_name == "heroku":
+            return HerokuCIAdapter()
+        if provider_name == "droneci":
+            return DroneCIAdapter()
+        if provider_name == "buildkite":
+            return BuildkiteAdapter()
+        if provider_name == "azurepipelines":
+            return AzurePipelinesCIAdapter()
+        if provider_name == "jenkins":
+            return JenkinsAdapter()
+        if provider_name == "cirrusci":
+            return CirrusCIAdapter()
     else:
         for provider in get_ci_providers_list():
             if provider.detect():

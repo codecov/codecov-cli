7438914fd7a5b3dc02c162ce1b43850c5a307478
Dana <dana@codecov.io>
2023-03-23 11:59:02 +0200

    tsting windows release

--- languages/languages.c
+++ languages/languages.c
@@ -2,18 +2,18 @@
 #include "Python.h"
 #include <wctype.h>
 
-static PyMethodDef module_methods[] = {};
+// static PyMethodDef module_methods[] = {};
 
-static struct PyModuleDef module_definition = {
-  .m_base = PyModuleDef_HEAD_INIT,
-  .m_name = "binding",
-  .m_doc = NULL,
-  .m_size = -1,
-  .m_methods = NULL,
-};
+// static struct PyModuleDef module_definition = {
+//   .m_base = PyModuleDef_HEAD_INIT,
+//   .m_name = "binding",
+//   .m_doc = NULL,
+//   .m_size = -1,
+//   .m_methods = NULL,
+// };
 
-PyMODINIT_FUNC PyInit_staticcodecov_languages(void) {
-  PyObject *module = PyModule_Create(&module_definition);
-  if (module == NULL) return NULL;
-  return module;
-}
+// PyMODINIT_FUNC PyInit_staticcodecov_languages(void) {
+//   PyObject *module = PyModule_Create(&module_definition);
+//   if (module == NULL) return NULL;
+//   return module;
+// }

2232a1acbfed1a0a7c2d6b56ed89a2f77423634f
Giovanni M Guidini <99758426+giovanni-guidini@users.noreply.github.com>
2023-04-20 22:30:29 +0200

    Introduce DAN Runner (#129)
    DAN (Do Anything Now), inspired from the chatGPT jailbreakers, is a runner that simply does what the user instructs.
    
    We are adding this because we anticipate having hundreds of different scenarios to deal with,
    and many different commands. You can think of this as having a custom runner without having to create one.
    It is a very power "override all commands" options, with no safety checks.
    
    We will depend heavily on documentation and user-instructing for using this.
    The idea is to give the user the power of what will be run when collecting tests and doing something with them.

--- codecov_cli/runners/__init__.py
+++ codecov_cli/runners/__init__.py
@@ -1,5 +1,6 @@
 import logging
 
+from codecov_cli.runners.dan_runner import DoAnythingNowRunner
 from codecov_cli.runners.python_standard_runner import PythonStandardRunner
 from codecov_cli.runners.types import LabelAnalysisRunnerInterface
 
@@ -18,6 +19,9 @@ def get_runner(cli_config, runner_name) -> LabelAnalysisRunnerInterface:
     if runner_name == "python":
         config_params = cli_config.get("runners", {}).get("python", {})
         return PythonStandardRunner(config_params)
+    elif runner_name == "dan":
+        config_params = cli_config.get("runners", {}).get("dan", {})
+        return DoAnythingNowRunner(config_params)
     logger.debug(
         f"Trying to load runner {runner_name}",
         extra=dict(
---
+++ tests/runners/test_dan_runner.py
@@ -0,0 +1,76 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from codecov_cli.runners.dan_runner import DoAnythingNowRunner
+from codecov_cli.runners.python_standard_runner import PythonStandardRunner
+
+
+class TestPythonStandardRunner(object):
+    @patch("codecov_cli.runners.dan_runner.subprocess.run")
+    def test_collect_tests(self, mock_run):
+        test_list = ["test_1", "test_2", "test_3"]
+        mock_stdout = MagicMock()
+        mock_stdout.configure_mock(
+            **{"stdout.decode.return_value": "\n".join(test_list)}
+        )
+        mock_run.return_value = mock_stdout
+
+        config_options = {"collect_tests_command": ["mycommand", "--option"]}
+        runner = DoAnythingNowRunner(config_options)
+        assert runner.params == config_options
+        resp = runner.collect_tests()
+        assert resp.split() == test_list
+        mock_run.assert_called_with(
+            ["mycommand", "--option"],
+            capture_output=True,
+            check=True,
+        )
+
+    def test_collect_test_no_config(self):
+        runner = DoAnythingNowRunner()
+        with pytest.raises(Exception) as exp:
+            runner.collect_tests()
+        assert (
+            str(exp.value)
+            == "DAN runner missing 'collect_tests_command' configuration value"
+        )
+
+    @patch("codecov_cli.runners.dan_runner.subprocess.run")
+    def test_process_labelanalysis_result(self, mock_run):
+        label_analysis_result = {
+            "present_report_labels": ["test_present"],
+            "absent_labels": ["test_absent"],
+            "present_diff_labels": ["test_in_diff"],
+            "global_level_labels": ["test_global"],
+        }
+        cmd_output = "My command output"
+        mock_stdout = MagicMock()
+        mock_stdout.configure_mock(**{"stdout.decode.return_value": cmd_output})
+        mock_run.return_value = mock_stdout
+        config_options = {
+            "process_labelanalysis_result_command": ["mycommand", "--option"]
+        }
+        runner = DoAnythingNowRunner(config_options)
+        runner.process_labelanalysis_result(label_analysis_result)
+        assert runner.params == config_options
+        mock_run.assert_called_with(
+            ["mycommand", "--option"],
+            capture_output=True,
+            check=True,
+        )
+
+    def test_process_labelanalysis_result_no_config(self):
+        label_analysis_result = {
+            "present_report_labels": ["test_present"],
+            "absent_labels": ["test_absent"],
+            "present_diff_labels": ["test_in_diff"],
+            "global_level_labels": ["test_global"],
+        }
+        runner = DoAnythingNowRunner()
+        with pytest.raises(Exception) as exp:
+            runner.process_labelanalysis_result(label_analysis_result)
+        assert (
+            str(exp.value)
+            == "DAN runner missing 'process_labelanalysis_result_command' configuration value"
+        )

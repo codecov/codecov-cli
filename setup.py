c774a4b0c6cf26cd6a4fce6bed5910a8f1ff7df6
Dana <dana@codecov.io>
2023-04-27 14:52:48 +0200

    add c header files to be recognized, add a step to build and publish to pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from platform import system
+from os import path
 
 from setuptools import Extension, find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="codecov-cli",
     version="0.1.4",
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     description="Codecov Command Line Interface",
-    long_description="",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Codecov",
     author_email="support@codecov.io",
     install_requires=["click", "requests", "PyYAML", "tree_sitter", "httpx"],
@@ -29,6 +36,8 @@ setup(
             include_dirs=[
                 "languages/treesitterpython/src",
                 "languages/treesitterjavascript/src",
+                "languages/treesitterjavascript/src/tree_sitter",
+                "languages/treesitterpython/src/tree_sitter",
             ],
             extra_compile_args=(
                 ["-Wno-unused-variable"] if system() != "Windows" else None

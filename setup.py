321d814aff8c98c17e5c8860cf4ba2603575ce85
Dana <dana@codecov.io>
2023-04-27 14:38:35 +0200

    testing

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="codecov-cli",
-    version="0.1.24",
+    version="0.1.25",
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     description="Codecov Command Line Interface",
     long_description=long_description,

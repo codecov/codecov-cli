f82952814321c5b789fcc07fd4b6aa5c3828d63a
Dana <dana@codecov.io>
2023-05-12 13:24:17 +0300

    move upload test to a seperate folder, modify logging

--- codecov_cli/services/empty_upload/__init__.py
+++ codecov_cli/services/empty_upload/__init__.py
@@ -15,7 +15,7 @@ def empty_upload_logic(commit_sha, slug, token, git_service):
     headers = get_token_header_or_fail(token)
     url = f"https://api.codecov.io/upload/{git_service}/{encoded_slug}/commits/{commit_sha}/empty-upload"
     sending_result = send_post_request(url=url, headers=headers)
-    log_warnings_and_errors_if_any(sending_result, "Empty Upload")
     if sending_result.status_code < 300 and sending_result.text:
         logger.info(sending_result.text)
+    log_warnings_and_errors_if_any(sending_result, "Empty Upload")
     return sending_result

e2415b836de8ba73f24a672cf5608d0bc428ba14
Dana <dana@codecov.io>
2023-05-19 18:38:23 +0300

    add complexity metrics to js functions, edit functions names to add class or functions wrappers

--- codecov_cli/services/staticanalysis/analyzers/general.py
+++ codecov_cli/services/staticanalysis/analyzers/general.py
@@ -1,6 +1,56 @@
-class GeneralAnalyzer(object):
+class BaseAnalyzer(object):
     def __init__(self, filename, actual_code):
         pass
 
     def process(self):
         return {}
+
+    def _count_elements(self, node, types):
+        count = 0
+        for c in node.children:
+            count += self._count_elements(c, types)
+        if node.type in types:
+            count += 1
+        return count
+
+    def _get_max_nested_conditional(self, node):
+        return (1 if node.type in self.condition_statements else 0) + max(
+            (self._get_max_nested_conditional(x) for x in node.children), default=0
+        )
+
+    def _get_complexity_metrics(self, body_node):
+        number_conditions = self._count_elements(
+            body_node,
+            self.condition_statements,
+        )
+        return {
+            "conditions": number_conditions,
+            "mccabe_cyclomatic_complexity": number_conditions + 1,
+            "returns": self._count_elements(body_node, ["return_statement"]),
+            "max_nested_conditional": self._get_max_nested_conditional(body_node),
+        }
+
+    def _get_name(self, node):
+        name_node = node.child_by_field_name("name")
+        actual_name = self.actual_code[
+            name_node.start_byte : name_node.end_byte
+        ].decode()
+        try:
+            wrapping_class = next(
+                x for x in self._get_parent_chain(node) if x.type in self.wrappers
+            )
+        except StopIteration:
+            wrapping_class = None
+        if wrapping_class is not None:
+            class_name_node = wrapping_class.child_by_field_name("name")
+            class_name = self.actual_code[
+                class_name_node.start_byte : class_name_node.end_byte
+            ].decode()
+            return f"{class_name}::{actual_name}"
+        return actual_name
+
+    def _get_parent_chain(self, node):
+        cur = node.parent
+        while cur:
+            yield cur
+            cur = cur.parent
--- samples/inputs/sample_003.js
+++ samples/inputs/sample_003.js
@@ -1,3 +1,98 @@
 function myFunction(p1, p2) {
   return p1 * p2;   // The function returns the product of p1 and p2
 }
+
+// Variable declaration and assignment
+let x = 10;
+const PI = 3.14159;
+var y = "Hello";
+
+// Function declaration and invocation
+function add(a, b) {
+  return a + b;
+}
+
+let result = add(x, 5);
+
+// Conditional statements
+if (x > 0) {
+  console.log("x is positive");
+} else if (x < 0) {
+  console.log("x is negative");
+} else {
+  console.log("x is zero");
+}
+
+// Looping constructs
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+let numbers = [1, 2, 3, 4, 5];
+for (let number of numbers) {
+  console.log(number);
+}
+
+// Objects and properties
+let person = {
+  name: "John",
+  age: 30,
+  address: {
+    street: "123 Main St",
+    city: "New York",
+  },
+};
+
+console.log(person.name);
+console.log(person.address.city);
+
+// Arrays and array methods
+let fruits = ["apple", "banana", "orange"];
+fruits.push("grape");
+console.log(fruits.length);
+
+// Classes and inheritance
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+
+  getColor() {
+    return this.color;
+  }
+}
+
+class Circle extends Shape {
+  constructor(color, radius) {
+    super(color);
+    this.radius = radius;
+  }
+
+  getArea() {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+let myCircle = new Circle("red", 5);
+console.log(myCircle.getColor());
+console.log(myCircle.getArea());
+
+// Promises and async/await
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Data received");
+    }, 2000);
+  });
+}
+
+async function getData() {
+  try {
+    let data = await fetchData();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getData();

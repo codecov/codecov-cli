989d490bcbaeee4b3d18b53d30fb070725aa217c
Gguidini <giovanni@codecov.io>
2023-05-23 17:05:26 +0200

    Add --max-wait-time option for label analysis
    This option allows customers to decide how long they want to way for the label analysis result
    (e.g. the set of labels to run) before falling back to running the entire test suite.
    
    I'm adding this option because on one occasion I've seen it taking more than 40min to get a response.
    No doubt some processing error happened and it failed silently, or something like that, and the user's
    CI just waited until it too was killed (for taking too long).
    
    One brute force solution is to just have a configurable time limit.
    Also taking this opportunity to refactor a little bit label analysis and improve coverage.

--- codecov_cli/commands/labelanalysis.py
+++ codecov_cli/commands/labelanalysis.py
@@ -1,12 +1,16 @@
 import logging
 import time
+from typing import List
 
 import click
 import requests
 
 from codecov_cli.fallbacks import CodecovOption, FallbackFieldEnum
 from codecov_cli.runners import get_runner
-from codecov_cli.runners.types import LabelAnalysisRequestResult
+from codecov_cli.runners.types import (
+    LabelAnalysisRequestResult,
+    LabelAnalysisRunnerInterface,
+)
 
 logger = logging.getLogger("codecovcli")
 
@@ -35,6 +39,13 @@ logger = logging.getLogger("codecovcli")
 @click.option(
     "--runner-name", "--runner", "runner_name", help="Runner to use", default="python"
 )
+@click.option(
+    "--max-wait-time",
+    "max_wait_time",
+    help="Max time (in seconds) to wait for the label analysis result before falling back to running all tests. Default is to wait forever.",
+    default=None,
+    type=int,
+)
 @click.pass_context
 def label_analysis(
     ctx: click.Context,
@@ -42,6 +53,7 @@ def label_analysis(
     head_commit_sha: str,
     base_commit_sha: str,
     runner_name: str,
+    max_wait_time: str,
 ):
     logger.debug(
         "Starting label analysis",
@@ -86,20 +98,8 @@ def label_analysis(
                 "Sorry. Codecov is having problems",
                 extra=dict(extra_log_attributes=dict(status_code=response.status_code)),
             )
-            if requested_labels:
-                logger.info(
-                    "Could not get set of tests to run. Falling back to running all collected tests."
-                )
-                fake_response = LabelAnalysisRequestResult(
-                    {
-                        "present_report_labels": [],
-                        "absent_labels": requested_labels,
-                        "present_diff_labels": [],
-                        "global_level_labels": [],
-                    }
-                )
-                return runner.process_labelanalysis_result(fake_response)
-            raise click.ClickException("Sorry. Codecov is having problems")
+            _fallback_to_collected_labels(requested_labels, runner)
+            return
         if response.status_code >= 400:
             logger.warning(
                 "Got a 4XX status code back from Codecov",
@@ -117,6 +117,7 @@ def label_analysis(
     eid = response.json()["external_id"]
     has_result = False
     logger.info("Label request sent. Waiting for result.")
+    start_wait = time.monotonic()
     time.sleep(2)
     while not has_result:
         resp_data = requests.get(
@@ -134,17 +135,36 @@ def label_analysis(
                 "Request had problems calculating",
                 extra=dict(extra_log_attributes=dict(resp_json=resp_json)),
             )
-            if requested_labels:
-                logger.info("Using requested labels as tests to run")
-                fake_response = LabelAnalysisRequestResult(
-                    {
-                        "present_report_labels": [],
-                        "absent_labels": requested_labels,
-                        "present_diff_labels": [],
-                        "global_level_labels": [],
-                    }
-                )
-                return runner.process_labelanalysis_result(fake_response)
+            _fallback_to_collected_labels(
+                collected_labels=requested_labels, runner=runner
+            )
+            return
+        if max_wait_time and (time.monotonic() - start_wait) > max_wait_time:
+            logger.error(
+                f"Exceeded max waiting time of {max_wait_time} seconds",
+            )
+            _fallback_to_collected_labels(
+                collected_labels=requested_labels, runner=runner
+            )
             return
         logger.info("Waiting more time for result")
         time.sleep(5)
+
+
+def _fallback_to_collected_labels(
+    collected_labels: List[str], runner: LabelAnalysisRunnerInterface
+) -> dict:
+    logger.info("Trying to fallback on collected labels")
+    if collected_labels:
+        logger.info("Using collected labels as tests to run")
+        fake_response = LabelAnalysisRequestResult(
+            {
+                "present_report_labels": [],
+                "absent_labels": collected_labels,
+                "present_diff_labels": [],
+                "global_level_labels": [],
+            }
+        )
+        return runner.process_labelanalysis_result(fake_response)
+    logger.error("Cannot fallback to collected labels because no labels were collected")
+    raise click.ClickException("Failed to get list of labels to run")

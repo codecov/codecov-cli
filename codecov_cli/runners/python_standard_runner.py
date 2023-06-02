c7c69c8c7e4980abad9193bda569099029bd27e3
Gguidini <giovanni@codecov.io>
2023-06-02 16:13:23 +0200

    Patch import errors (the return of subprocess.run)
    Previously (around commit 8c5b289c90a6be8f53eb13babb2a19ef24e1b6ab) we had removed `subprocess.run` from the
    `PythonStandardRunner` to make it extra safe. Instead the default way of using it was calling `pytest` directly
    from Python.
    
    The thing is that Python import process when running pytest from Python can get messy. Because imported modules
    are cached. Out of the blue we started seeing "pytest.PytestAssertRewriteWarning" errors.
    The most likely cause is that some modules are imported by the CLI, and when the process that runs Pytest is created
    it inherits some of the parent's process resources ([because it's forked](https://docs.python.org/3/library/multiprocessing.html#contexts-and-start-methods)). Yes I tried with "spawn" instead of "fork" but that didn't work either /:
    
    However `subprocess.run` (which probably works differently under the hood?) does work.
    It's better to have something slightly more dangerous but that is more reliable in working, than continue to try and make
    the safer option work (which (1) is starting to feel hacky and (2) I am out of ideas and we want to ship this feature rather
    soon). So the logical choice is to make `subprocess.run` the default way of running pytest. It's also way simpler.
    
    Nevertheless, in the process of trying to make the (now called) strict option work I made some improvement to it in terms
    of failing when the subprocess fails. Before it was gettign stuck trying to read from the queue. Now it fails right away.
    
    Ans yes, instead of just throwing away the previous approach we're now calling it "strict mode".

--- codecov_cli/runners/python_standard_runner.py
+++ codecov_cli/runners/python_standard_runner.py
@@ -1,9 +1,12 @@
 import logging
 import random
+import subprocess
 from contextlib import redirect_stdout
 from io import StringIO, TextIOWrapper
-from multiprocessing import Queue, get_context
+from multiprocessing import Process, Queue, get_context
 from os import getcwd
+from queue import Empty
+from subprocess import CalledProcessError
 from sys import path, stdout
 from typing import List, Optional
 
@@ -23,8 +26,19 @@ class PythonStandardRunnerConfigParams(dict):
         return self.get("collect_tests_options", [])
 
     @property
+    def strict_mode(self) -> bool:
+        """
+        Run pytest from within Python instead of using subprocess.run
+        This is potentailly safer than using subprocess.run because it guarantees better that
+        the program running is indeed pytest.
+        But it might not work everytime due to import issues related to Python caching modules.
+        """
+        return self.get("strict_mode", False)
+
+    @property
     def include_curr_dir(self) -> bool:
-        """ "
+        """
+        Only valid for 'strict mode'
         Account for the difference 'pytest' vs 'python -m pytest'
         https://docs.pytest.org/en/7.1.x/how-to/usage.html#calling-pytest-through-python-m-pytest
         """
@@ -32,6 +46,11 @@ class PythonStandardRunnerConfigParams(dict):
 
 
 def _include_curr_dir(method):
+    """
+    Account for the difference 'pytest' vs 'python -m pytest'
+    https://docs.pytest.org/en/7.1.x/how-to/usage.html#calling-pytest-through-python-m-pytest
+    """
+
     def call_method(self, *args, **kwargs):
         include_curr_dir = self.params.include_curr_dir
         curr_dir = getcwd()
@@ -64,8 +83,8 @@ def _execute_pytest_subprocess(
     with redirect_stdout(subproces_stdout):
         result = pytest.main(pytest_args)
     if capture_output:
-        queue.put(subproces_stdout.getvalue())
-    queue.put(result)
+        queue.put({"output": subproces_stdout.getvalue()})
+    queue.put({"result": result})
 
 
 class PythonStandardRunner(LabelAnalysisRunnerInterface):
@@ -75,26 +94,40 @@ class PythonStandardRunner(LabelAnalysisRunnerInterface):
             config_params = {}
         self.params = PythonStandardRunnerConfigParams(config_params)
 
+    def _wait_pytest(self, pytest_process: Process, queue: Queue):
+        pytest_process.start()
+        result = None
+        output = None
+        while pytest_process.exitcode == 0 or pytest_process.exitcode == None:
+            from_queue = None
+            try:
+                from_queue = queue.get(timeout=1)
+            except Empty:
+                pass
+            if from_queue and "output" in from_queue:
+                output = from_queue["output"]
+            if from_queue and "result" in from_queue:
+                result = from_queue["result"]
+            if result is not None:
+                break
+        pytest_process.join()
+        return result, output
+
     @_include_curr_dir
-    def _execute_pytest(
-        self, pytest_args: List[str], capture_output: bool = True, numprocess: int = 1
+    def _execute_pytest_strict(
+        self, pytest_args: List[str], capture_output: bool = True
     ) -> str:
         """Handles calling pytest from Python in a subprocess.
         Raises Exception if pytest fails
         Returns the complete pytest output
         """
-        ctx = get_context("fork")
-        output = None
+        ctx = get_context(method="fork")
         queue = ctx.Queue(2)
         p = ctx.Process(
             target=_execute_pytest_subprocess,
             args=[pytest_args, queue, stdout, capture_output],
         )
-        p.start()
-        if capture_output:
-            output = queue.get()  # Output from pytest emitted by subprocess
-        result = queue.get()  # Pytest exit code emitted by subprocess
-        p.join()
+        result, output = self._wait_pytest(p, queue)
 
         if p.exitcode != 0 or (result != pytest.ExitCode.OK and result != 0):
             logger.error(
@@ -103,6 +136,29 @@ class PythonStandardRunner(LabelAnalysisRunnerInterface):
             )
             raise Exception("Pytest did not run correctly")
         return output
+
+    def _execute_pytest(self, pytest_args: List[str], capture_output: bool = True):
+        """Handles calling pytest using subprocess.run.
+        Raises Exception if pytest fails
+        Returns the complete pytest output
+        """
+        command = ["python", "-m", "pytest"] + pytest_args
+        try:
+            result = subprocess.run(
+                command,
+                capture_output=capture_output,
+                check=True,
+                stdout=(stdout if not capture_output else None),
+            )
+        except CalledProcessError as exp:
+            logger.error(exp.stderr)
+            logger.error(
+                "Pytest did not run correctly",
+                extra=dict(extra_log_attributes=dict(exit_code=exp.returncode)),
+            )
+            raise Exception("Pytest did not run correctly")
+        if capture_output:
+            return result.stdout.decode()
 
     def collect_tests(self):
         default_options = ["-q", "--collect-only"]
@@ -115,7 +171,10 @@ class PythonStandardRunner(LabelAnalysisRunnerInterface):
             ),
         )
 
-        output = self._execute_pytest(options_to_use)
+        if self.params.strict_mode:
+            output = self._execute_pytest_strict(options_to_use)
+        else:
+            output = self._execute_pytest(options_to_use)
         lines = output.split(sep="\n")
         test_names = list(line for line in lines if ("::" in line and "test" in line))
         return test_names
@@ -161,6 +220,9 @@ class PythonStandardRunner(LabelAnalysisRunnerInterface):
             "Pytest command",
             extra=dict(extra_log_attributes=dict(command_array=command_array)),
         )
-        output = self._execute_pytest(command_array, capture_output=False)
+        if self.params.strict_mode:
+            output = self._execute_pytest_strict(command_array, capture_output=False)
+        else:
+            output = self._execute_pytest(command_array, capture_output=False)
         logger.info("Finished running tests successfully")
         logger.debug(output)

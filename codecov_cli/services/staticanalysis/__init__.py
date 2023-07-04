5e57104bf41fb203baefcf3b3f9e04ebdbfd503d
joseph-sentry <joseph.sawaya@sentry.io>
2023-07-04 09:39:23 -0400

    Move sending finish signal to its own function
    This commit also adds the response status code to the log
    after sending the finish signal.
    
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- codecov_cli/services/staticanalysis/__init__.py
+++ codecov_cli/services/staticanalysis/__init__.py
@@ -140,26 +140,12 @@ async def run_analysis_entrypoint(
     else:
         logger.info("All files are already uploaded!")
     try:
-        external_id = response_json["external_id"]
-        logger.debug(
-            "Sending finish signal to let API know to schedule static analysis task",
-            extra=dict(extra_log_attributes=dict(external_id=external_id)),
-        )
-        upload_url = enterprise_url or CODECOV_API_URL
-        response = requests.post(
-            f"{upload_url}/staticanalysis/analyses/{external_id}/finish",
-            headers={"Authorization": f"Repotoken {token}"},
-        )
-        if response.status_code >= 500:
-            raise click.ClickException("Sorry. Codecov is having problems")
-        if response.status_code >= 400:
-            raise click.ClickException(
-                f"There is some problem with the submitted information.\n{response_json.get('detail')}"
-            )
+        response = send_finish_signal(response_json, upload_url, token)
     except requests.RequestException:
         raise click.ClickException(click.style("Unable to reach Codecov", fg="red"))
     logger.info(
-        "Received response from server",
+        "Received response with status code %s from server",
+        response.status_code,
         extra=dict(
             extra_log_attributes=dict(time_taken=response.elapsed.total_seconds())
         ),
@@ -198,6 +184,25 @@ async def send_single_upload_put(client, all_data, el):
         "filepath": el["filepath"],
         "succeeded": False,
     }
+
+
+def send_finish_signal(response_json, upload_url: str, token: str):
+    external_id = response_json["external_id"]
+    logger.debug(
+        "Sending finish signal to let API know to schedule static analysis task",
+        extra=dict(extra_log_attributes=dict(external_id=external_id)),
+    )
+    response = requests.post(
+        f"{upload_url}/staticanalysis/analyses/{external_id}/finish",
+        headers={"Authorization": f"Repotoken {token}"},
+    )
+    if response.status_code >= 500:
+        raise click.ClickException("Sorry. Codecov is having problems")
+    if response.status_code >= 400:
+        raise click.ClickException(
+            f"There is some problem with the submitted information.\n{response_json.get('detail')}"
+        )
+    return response
 
 
 def analyze_file(config, filename: FileAnalysisRequest):

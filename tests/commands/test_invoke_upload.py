3ce072043e1ca7250b067951363e4b44eaa3ee01
Gguidini <giovanni@codecov.io>
2023-07-18 16:44:57 +0200

    Enable ATS in the CLI
    Changing tests that used the `-v` option (invoking the CLI) to instead use the fixture.
    This prevents tests from messing up the global env across tests when running pytest using ATS.
    As we've been discovering recently, it's a big mess sometimes.
    
    The removal of "MASKED" from some git tests is because it was throwing errors in CI. Maybe it's a python 3.11.x thing...
    But I suppose they changed when the removal of secrets run. I changed for bogus secrets.
    
    THe git services was also an error, so I had to put them in a list.

--- tests/commands/test_invoke_upload.py
+++ tests/commands/test_invoke_upload.py
@@ -2,13 +2,13 @@ from click.testing import CliRunner
 
 from codecov_cli.fallbacks import FallbackFieldEnum
 from codecov_cli.main import cli
-from codecov_cli.services.upload import UploadSender
+from codecov_cli.services.upload import UploadCollector, UploadSender
 from codecov_cli.types import RequestError, RequestResult
 from tests.factory import FakeProvider, FakeVersioningSystem
 from tests.test_helpers import parse_outstreams_into_log_lines
 
 
-def test_upload_missing_commit_sha(mocker):
+def test_upload_missing_commit_sha(mocker, use_verbose_option):
     fake_ci_provider = FakeProvider({FallbackFieldEnum.commit_sha: None})
     fake_versioning_system = FakeVersioningSystem({FallbackFieldEnum.commit_sha: None})
     mocker.patch(
@@ -16,7 +16,7 @@ def test_upload_missing_commit_sha(mocker):
     )
     mocker.patch("codecov_cli.main.get_ci_adapter", return_value=fake_ci_provider)
     runner = CliRunner()
-    result = runner.invoke(cli, ["-v", "do-upload"], obj={})
+    result = runner.invoke(cli, ["do-upload"], obj={})
     assert result.exit_code != 0
     print(result.output)
     assert "Missing option '-C' / '--sha' / '--commit-sha'" in result.output
@@ -32,9 +32,14 @@ def test_upload_raise_Z_option(mocker):
     upload_sender = mocker.patch.object(
         UploadSender, "send_upload_data", return_value=result
     )
+    upload_collector = mocker.patch.object(UploadCollector, "generate_upload_data")
+    fake_ci_provider = FakeProvider({FallbackFieldEnum.commit_sha: None})
+    mocker.patch("codecov_cli.main.get_ci_adapter", return_value=fake_ci_provider)
+
     runner = CliRunner()
     result = runner.invoke(cli, ["do-upload", "--fail-on-error"], obj={})
-    upload_sender.assert_called
+    upload_sender.assert_called()
+    upload_collector.assert_called()
     assert ("error", "Upload failed: Unauthorized") in parse_outstreams_into_log_lines(
         result.output
     )

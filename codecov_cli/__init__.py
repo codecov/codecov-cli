797337d9b2c3d9e250a2c70e8d0987e0be2579dc
Gguidini <giovanni@codecov.io>
2023-10-05 14:31:24 -0300

    hotfix: previous change still yields a broken release. So going back to manually editing the version number. We will fix the release process shortly

--- codecov_cli/__init__.py
+++ codecov_cli/__init__.py
@@ -1,9 +1,1 @@
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-
-with open(path.join(here, "..", "VERSION"), encoding="utf-8") as f:
-    version_number = f.readline().strip()
-
-__version__ = version_number
+__version__ = '0.3.6'

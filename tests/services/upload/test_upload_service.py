8fccee6b8184bcd0063837a4cbb7ba6162f466e3
joseph-sentry <joseph.sawaya@sentry.io>
2024-01-03 09:57:34 -0500

    tests: add tests for test_results
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- tests/services/upload/test_upload_service.py
+++ tests/services/upload/test_upload_service.py
@@ -467,3 +467,90 @@ def test_do_upload_rase_no_cov_reports_found_error(mocker):
     mock_select_file_finder.assert_called_with(None, None, None, False)
     mock_select_network_finder.assert_called_with(versioning_system)
     mock_generate_upload_data.assert_called_with("coverage")
+
+
+def test_do_upload_logic_happy_path_test_results(mocker):
+    mock_select_preparation_plugins = mocker.patch(
+        "codecov_cli.services.upload.select_preparation_plugins"
+    )
+    mock_select_file_finder = mocker.patch(
+        "codecov_cli.services.upload.select_file_finder"
+    )
+    mock_select_network_finder = mocker.patch(
+        "codecov_cli.services.upload.select_network_finder"
+    )
+    mock_generate_upload_data = mocker.patch.object(
+        UploadCollector, "generate_upload_data"
+    )
+    mock_send_upload_data = mocker.patch.object(
+        UploadSender,
+        "send_upload_data",
+        return_value=UploadSendingResult(
+            error=None,
+            warnings=[UploadSendingResultWarning(message="somewarningmessage")],
+        ),
+    )
+    cli_config = {}
+    versioning_system = mocker.MagicMock()
+    ci_adapter = mocker.MagicMock()
+    ci_adapter.get_fallback_value.return_value = "service"
+    runner = CliRunner()
+    with runner.isolation() as outstreams:
+        res = do_upload_logic(
+            cli_config,
+            versioning_system,
+            ci_adapter,
+            upload_file_type="test_results",
+            commit_sha="commit_sha",
+            report_code="report_code",
+            build_code="build_code",
+            build_url="build_url",
+            job_code="job_code",
+            env_vars=None,
+            flags=None,
+            name="name",
+            network_root_folder=None,
+            files_search_root_folder=None,
+            files_search_exclude_folders=None,
+            files_search_explicitly_listed_files=None,
+            plugin_names=["first_plugin", "another", "forth"],
+            token="token",
+            branch="branch",
+            slug="slug",
+            pull_request_number="pr",
+            git_service="git_service",
+            enterprise_url=None,
+        )
+    out_bytes = parse_outstreams_into_log_lines(outstreams[0].getvalue())
+    assert out_bytes == [
+        ("info", "Process Upload complete"),
+        ("info", "Upload process had 1 warning"),
+        ("warning", "Warning 1: somewarningmessage"),
+    ]
+
+    assert res == UploadSender.send_upload_data.return_value
+    mock_select_preparation_plugins.assert_called_with(
+        cli_config, ["first_plugin", "another", "forth"]
+    )
+    mock_select_file_finder.assert_called_with(None, None, None, False)
+    mock_select_network_finder.assert_called_with(versioning_system)
+    mock_generate_upload_data.assert_called_with("test_results")
+    mock_send_upload_data.assert_called_with(
+        mock_generate_upload_data.return_value,
+        "commit_sha",
+        "token",
+        None,
+        "report_code",
+        "test_results",
+        "name",
+        "branch",
+        "slug",
+        "pr",
+        "build_code",
+        "build_url",
+        "job_code",
+        None,
+        "service",
+        "git_service",
+        None,
+    )

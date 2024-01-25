f3827b3ed915f689c425b8d3986b28835973318f
Codecov Releaser <devops+releaser@codecov.io>
2024-01-25 12:25:03 -0500

    Prepare release 0.4.5 (#358)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="codecov-cli",
-    version="0.4.4",
+    version="0.4.5",
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     description="Codecov Command Line Interface",
     long_description=long_description,

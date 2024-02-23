956761c7360bb6db4d69cc2d99a1e79e59c398a6
Tom Hu <thomas.hu@sentry.io>
2024-02-23 15:43:49 -0800

    fix: set safe.directory for git

--- codecov_cli/helpers/ci_adapters/github_actions.py
+++ codecov_cli/helpers/ci_adapters/github_actions.py
@@ -1,14 +1,36 @@
+import logging
 import os
 import re
 import subprocess
 
 from codecov_cli.helpers.ci_adapters.base import CIAdapterBase
 
+logger = logging.getLogger("codecovcli")
+
 
 class GithubActionsCIAdapter(CIAdapterBase):
     # https://docs.github.com/en/actions/learn-github-actions/environment-variables
     def detect(self) -> bool:
-        return bool(os.getenv("GITHUB_ACTIONS"))
+        detected = bool(os.getenv("GITHUB_ACTIONS"))
+        if detected:
+            self._set_safe_directory()
+        return detected
+
+    # In Docker containers, GitHub Actions will checkout the repo as a different user
+    # https://github.com/actions/checkout/issues/760
+    # In order for future `git` operations to work, it must set the safe directory
+    # Note that the checkout step should be running this
+    # https://github.com/actions/checkout/blob/b32f140b0c872d58512e0a66172253c302617b90/src/git-source-provider.ts#L52
+    def _set_safe_directory(self):
+        is_safe_directory = subprocess.run(
+            ["git", "config", "--get", "safe.directory"], capture_output=True
+        )
+        if not is_safe_directory:
+            safe_directory = os.path.join("__w", self._get_slug())
+            logger.info(f"  Setting safe.directory with git config --global --add safe.directory {safe_directory}")
+            subprocess.run(
+               ["git", "config", "--global", "-add", "safe.directory", safe_directory]
+            )
 
     def _get_commit_sha(self):
         pr = self._get_pull_request_number()

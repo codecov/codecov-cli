badd8b79d393ae873a6ba7ce79c32a7b708da3b7
Tom Hu <thomas.hu@sentry.io>
2024-04-16 14:27:53 -0700

    fix: cleanup again

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="codecov-cli",
-    version="0.5.2",
+    version="0.5.1",
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     description="Codecov Command Line Interface",
     long_description=long_description,

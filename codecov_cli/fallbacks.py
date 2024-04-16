a051e181bd15f9f0141beeb6d610167becee87ac
Tom Hu <thomas.hu@sentry.io>
2024-04-16 14:14:21 -0700

    fix: add name to fallbacks

--- codecov_cli/fallbacks.py
+++ codecov_cli/fallbacks.py
@@ -5,15 +5,16 @@ import click
 
 
 class FallbackFieldEnum(Enum):
-    commit_sha = auto()
-    build_url = auto()
-    build_code = auto()
-    job_code = auto()
-    pull_request_number = auto()
-    slug = auto()
     branch = auto()
-    service = auto()
+    build_code = auto()
+    build_url = auto()
+    commit_sha = auto()
     git_service = auto()
+    job_code = auto()
+    name = auto()
+    pull_request_number = auto()
+    service = auto()
+    slug = auto()
 
 
 class CodecovOption(click.Option):

fa051b974e5308f50926854936235f2ec858b38b
joseph-sentry <joseph.sawaya@sentry.io>
2024-05-10 16:12:39 -0400

    test: fix tests
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- tests/commands/test_process_test_results.py
+++ tests/commands/test_process_test_results.py
@@ -17,8 +17,9 @@ def test_process_test_results(
         os.environ,
         {
             "GITHUB_REPOSITORY": "fake/repo",
-            "GITHUB_REF": "pull/fake/pull",
+            "GITHUB_REF": "refs/pull/1/merge",
             "GITHUB_STEP_SUMMARY": tmp_file.dirname + tmp_file.basename,
+            "GITHUB_SERVER_URL": "https://github.com",
         },
     )
     mocked_post = mocker.patch(
@@ -44,9 +45,9 @@ def test_process_test_results(
     assert result.exit_code == 0
 
     mocked_post.assert_called_with(
-        url="https://api.github.com/repos/fake/repo/issues/pull/comments",
+        url="https://api.github.com/repos/fake/repo/issues/1/comments",
         data={
-            "body": "### :x: Failed Test Results: \nCompleted 4 tests with **`1 failed`**, 3 passed and 0 skipped.\n<details><summary>View the full list of failed tests</summary>\n\n| **Test Description** | **Failure message** |\n| :-- | :-- |\n| <pre>Testsuite:<br>api.temp.calculator.test_calculator::test_divide<br><br>Test name:<br>pytest<br></pre> | <pre>def<br>                test_divide():<br>                &amp;gt; assert Calculator.divide(1, 2) == 0.5<br>                E assert 1.0 == 0.5<br>                E + where 1.0 = &amp;lt;function Calculator.divide at 0x104c9eb90&amp;gt;(1, 2)<br>                E + where &amp;lt;function Calculator.divide at 0x104c9eb90&amp;gt; = Calculator.divide<br>                .../temp/calculator/test_calculator.py:30: AssertionError</pre> |\n<!-- Codecov comment -->"
+            "body": "### :x: Failed Test Results: \nCompleted 4 tests with **`1 failed`**, 3 passed and 0 skipped.\n<details><summary>View the full list of failed tests</summary>\n\n| **Test Description** | **Failure message** |\n| :-- | :-- |\n| <pre>Testsuite:<br>api.temp.calculator.test_calculator::test_divide<br><br>Test name:<br>pytest<br></pre> | <pre>def<br>                test_divide():<br>                &amp;gt; assert Calculator.divide(1, 2) == 0.5<br>                E assert 1.0 == 0.5<br>                E + where 1.0 = &amp;lt;function Calculator.divide at 0x104c9eb90&amp;gt;(1, 2)<br>                E + where &amp;lt;function Calculator.divide at 0x104c9eb90&amp;gt; = Calculator.divide<br>                .../temp/calculator/test_calculator.py:30: AssertionError</pre> |\n[View checks](https://github.com/fake/repo/pull/1/checks\n<!-- Codecov comment -->"
         },
         headers={
             "Accept": "application/vnd.github+json",

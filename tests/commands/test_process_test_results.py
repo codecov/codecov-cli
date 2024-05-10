dcfad1a4327b5026451c47b2537fe846fb7b3996
joseph-sentry <joseph.sawaya@sentry.io>
2024-05-10 13:04:51 -0400

    test: fix tests
    Signed-off-by: joseph-sentry <joseph.sawaya@sentry.io>

--- tests/commands/test_process_test_results.py
+++ tests/commands/test_process_test_results.py
@@ -1,4 +1,3 @@
-import logging
 import os
 
 from click.testing import CliRunner
@@ -47,7 +46,7 @@ def test_process_test_results(
     mocked_post.assert_called_with(
         url="https://api.github.com/repos/fake/repo/issues/pull/comments",
         data={
-            "body": "### :x: Failed Test Results: \nCompleted 4 tests with **`1 failed`**, 3 passed and 0 skipped.\n<details><summary>View the full list of failed tests</summary>\n\n| **Test Description** | **Failure message** |\n| :-- | :-- |\n| <pre>Testsuite:<br>api.temp.calculator.test_calculator::test_divide<br><br>Test name:<br>pytest<br></pre> | <pre>def<br>                test_divide():<br>                &amp;gt; assert Calculator.divide(1, 2) == 0.5<br>                E assert 1.0 == 0.5<br>                E + where 1.0 = &amp;lt;function Calculator.divide at 0x104c9eb90&amp;gt;(1, 2)<br>                E + where &amp;lt;function Calculator.divide at 0x104c9eb90&amp;gt; = Calculator.divide<br>                .../temp/calculator/test_calculator.py:30: AssertionError</pre> |"
+            "body": "### :x: Failed Test Results: \nCompleted 4 tests with **`1 failed`**, 3 passed and 0 skipped.\n<details><summary>View the full list of failed tests</summary>\n\n| **Test Description** | **Failure message** |\n| :-- | :-- |\n| <pre>Testsuite:<br>api.temp.calculator.test_calculator::test_divide<br><br>Test name:<br>pytest<br></pre> | <pre>def<br>                test_divide():<br>                &amp;gt; assert Calculator.divide(1, 2) == 0.5<br>                E assert 1.0 == 0.5<br>                E + where 1.0 = &amp;lt;function Calculator.divide at 0x104c9eb90&amp;gt;(1, 2)<br>                E + where &amp;lt;function Calculator.divide at 0x104c9eb90&amp;gt; = Calculator.divide<br>                .../temp/calculator/test_calculator.py:30: AssertionError</pre> |\n<!-- Codecov comment -->"
         },
         headers={
             "Accept": "application/vnd.github+json",

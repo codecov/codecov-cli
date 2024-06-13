5cc11114f44312ea7dc67d13398eddd4db99346e
Sasha Gerrand <sgerrand@users.noreply.github.com>
2024-06-13 16:01:21 +0100

    Add additional `codecov` entrypoint
    Given that the https://github.com/codecov/codecov-python repository was archived last year and the associated Python package is deprecated, adding `codecov` as en entrypoint would make this Python package's script names more aligned to the other, operating system specific, binary executables which are created from the same code. That would also make life easier for any maintainers who may want to package this project as part of a software distribution like Homebrew.

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
+            "codecov = codecov_cli.main:run",
             "codecovcli = codecov_cli.main:run",
         ],
     },

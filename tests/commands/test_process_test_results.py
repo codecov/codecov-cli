1478514b1203d1c3921ca60b767c4224d0c417e4
joseph-sentry <136376984+joseph-sentry@users.noreply.github.com>
2024-08-02 12:05:41 -0400

    Revert "feat: disable search means don't search for explicitly mentioned file…" (#480)
    This reverts commit 8810ef7fc2bb5662eb53a0582fc41f6e8b296508.

--- tests/commands/test_process_test_results.py
+++ tests/commands/test_process_test_results.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from click.testing import CliRunner
@@ -10,6 +11,7 @@ def test_process_test_results(
     mocker,
     tmpdir,
 ):
+
     tmp_file = tmpdir.mkdir("folder").join("summary.txt")
 
     mocker.patch.dict(
@@ -42,6 +44,7 @@ def test_process_test_results(
 
     assert result.exit_code == 0
 
+
     mocked_post.assert_called_with(
         url="https://api.github.com/repos/fake/repo/issues/pull/comments",
         data={
@@ -53,6 +56,7 @@ def test_process_test_results(
             "X-GitHub-Api-Version": "2022-11-28",
         },
     )
+
 
 
 def test_process_test_results_non_existent_file(mocker, tmpdir):
@@ -89,7 +93,7 @@ def test_process_test_results_non_existent_file(mocker, tmpdir):
     assert result.exit_code == 1
     expected_logs = [
         "ci service found",
-        "No JUnit XML files were found",
+        'Some files were not found',
     ]
     for log in expected_logs:
         assert log in result.output
@@ -176,6 +180,7 @@ def test_process_test_results_missing_ref(mocker, tmpdir):
     ]
     for log in expected_logs:
         assert log in result.output
+
 
 
 def test_process_test_results_missing_step_summary(mocker, tmpdir):

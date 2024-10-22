199872e081ff928162dc42f50a8bb576272d907a
Tom Hu <thomas.hu@sentry.io>
2024-10-22 15:45:10 -0500

    fix: bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="codecov-cli",
-    version="0.8.0",
+    version="0.9.0-beta",
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     description="Codecov Command Line Interface",
     long_description=long_description,

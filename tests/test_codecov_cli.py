34875e3b8c36e34afdb8b624393b37a748a2d51a
Tony Le <tony.le@sentry.io>
2024-11-06 21:53:23 -0500

    Add command

--- tests/test_codecov_cli.py
+++ tests/test_codecov_cli.py
@@ -3,6 +3,7 @@ from codecov_cli import main
 
 def test_existing_commands():
     assert sorted(main.cli.commands.keys()) == [
+        "combined-upload",
         "create-commit",
         "create-report",
         "create-report-results",

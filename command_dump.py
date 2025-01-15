a031977e97ccaf7040dee5e2a1e1f39933f547b8
Tom Hu <88201630+thomasrockhu-codecov@users.noreply.github.com>
2025-01-15 11:09:05 -0300

    chore: dump the full command helptext (#587)
    * fix: first pass
    
    * fix: try click man
    
    * fix: add it into ci
    
    * fix: update the detection script
    
    * fix: error

---
+++ command_dump.py
@@ -0,0 +1,31 @@
+import subprocess
+
+
+def command_dump(commands):
+    print(f"Dumping {' '.join(commands)}")
+    command_docs = subprocess.run(
+        [*commands, "--help"], capture_output=True
+    ).stdout.decode("utf-8")
+
+    split_docs = command_docs.split("\n")
+
+    try:
+        index_of = split_docs.index("Commands:")
+    except ValueError:
+        return command_docs
+
+    if "Commands:" in split_docs:
+        sub_commands = [
+            sub_command.strip() for sub_command in split_docs[index_of + 1 :] if sub_command.strip()
+        ]
+        for sub_command in sub_commands:
+            command_docs = "\n".join(
+                [command_docs, command_dump([*commands, sub_command])]
+            )
+
+    with open("codecovcli_commands", "w") as f:
+        f.write(command_docs)
+
+
+if __name__ == "__main__":
+    command_dump(["codecovcli"])

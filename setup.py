e2eae5e7c8d208bb54cf3847e4e24781cb6566f4
Tom Hu <thomas.hu@sentry.io>
2025-01-21 14:26:44 -0300

    fix: force back to 10.0.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="codecov-cli",
-    version="10.0.1",
+    version="10.0.0",
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     description="Codecov Command Line Interface",
     long_description=long_description,

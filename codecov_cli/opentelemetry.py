a54b07c96fc090bb77f3da6f29a64ea34e7e413f
Tom Hu <thomas.hu@sentry.io>
2025-01-26 20:52:16 -0300

    fix: use CODECOV_ENV

--- codecov_cli/opentelemetry.py
+++ codecov_cli/opentelemetry.py
@@ -11,6 +11,8 @@ def init_telem(ctx):
         return
     if ctx['enterprise_url']:  # dont run on dedicated cloud
         return
+    if os.getenv('CODECOV_ENV', 'production') == 'test':
+        return
 
     sentry_sdk.init(
         dsn="https://0bea75c61745c221a6ef1ac1709b1f4d@o26192.ingest.us.sentry.io/4508615876083713",

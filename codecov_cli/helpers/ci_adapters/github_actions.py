b69e396f7e673e333732b74179da871c8e476b08
Joseph Sawaya <joseph.sawaya@sentry.io>
2025-05-01 11:00:21 -0400

    fix: match previous logic

--- codecov_cli/helpers/ci_adapters/github_actions.py
+++ codecov_cli/helpers/ci_adapters/github_actions.py
@@ -1,7 +1,6 @@
 import os
 import re
 import subprocess
-
 from codecov_cli.helpers.ci_adapters.base import CIAdapterBase
 
 
@@ -66,10 +65,10 @@ class GithubActionsCIAdapter(CIAdapterBase):
         return os.getenv("GITHUB_REPOSITORY")
 
     def _get_branch(self):
-        def remove_prefix(s, prefix):
+        def remove_prefix(s: str, prefix: str) -> str:
             if s.startswith(prefix):
                 return s[len(prefix) :]
-            return s
+            return ""
 
         head_ref = os.getenv("GITHUB_HEAD_REF", "")
         ref = remove_prefix(os.getenv("GITHUB_REF", ""), "refs/heads/")

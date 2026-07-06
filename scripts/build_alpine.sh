76e458dc22d20282b1f9734dc2e368e831a137a3
Calvin Yau <calvin.yau@harness.io>
2026-07-06 12:36:13 -0700

    chore: Fix build_alpine uv install

--- scripts/build_alpine.sh
+++ scripts/build_alpine.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eux
 apk add build-base python3 py3-pip curl
-python3 -m pip install --break-system-packages uv
+python3 -m pip install uv --only-binary uv
 # Need to build with python 3.9 to support systems with libpython >= 3.9
 uv python pin 3.9
 uv sync --no-binary-package pyyaml --no-binary-package ijson
